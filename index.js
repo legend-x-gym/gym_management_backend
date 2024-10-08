0fd67db3aa417ab5df37e522f47a657b3cfbbbd6
YonasWorkneh <yonasdegefu158@gmail.com>
2024-10-08 08:37:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 4000;
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
